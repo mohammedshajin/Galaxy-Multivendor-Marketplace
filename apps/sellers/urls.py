aa995e834a92034d463e1a5d9227fafe76d5ecfc
mohammed shajin <mohammedshajin1997@gmail.com>
2021-05-22 16:51:41 +0530

    Add product feature added

--- apps/sellers/urls.py
+++ apps/sellers/urls.py
@@ -5,6 +5,8 @@ from.import views
 urlpatterns = [
     path('become-seller/', views.become_seller, name='become_seller'),
     path('seller-admin/', views.seller_admin, name='seller_admin'),
+    path('add_product/', views.add_product, name='add_product'),
+
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
    
--- apps/sellers/views.py
+++ apps/sellers/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import redirect, render
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
+from django.utils.text import slugify
 from django.contrib.auth.decorators import login_required
 from apps.product.models import Product  
 from.models import Seller
+from.forms import ProductForm
 
 def become_seller(request):
     if request.method == 'POST':
@@ -26,4 +28,19 @@ def seller_admin(request):
     products = seller.products.all()
     return render(request, 'seller_admin.html', {'seller': seller, 'products':products})
 
-# Create your views here.
+@login_required
+def add_product(request):
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+
+        if form.is_valid():
+            product = form.save(commit=False)
+            product.seller = request.user.seller
+            product.slug = slugify(product.title)
+            product.save()
+
+            return redirect('vendor_admin')
+    else:
+        form = ProductForm()
+    
+    return render(request, 'add_product.html', {'form': form})
