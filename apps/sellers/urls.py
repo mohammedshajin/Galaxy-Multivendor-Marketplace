92b33ef963223f76e000a9c7cf23f77bf752adf9
mohammed shajin <mohammedshajin1997@gmail.com>
2021-05-22 15:12:43 +0530

    login feature added

--- apps/sellers/urls.py
+++ apps/sellers/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('become-seller/', views.become_seller, name='become_seller'),
     path('seller-admin/', views.seller_admin, name='seller_admin'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
    
 ]
