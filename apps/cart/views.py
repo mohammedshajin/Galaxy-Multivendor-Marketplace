472b2ab59fcad573e1ba01fd4b6ccdcc740687d4
mohammed shajin <mohammedshajin1997@gmail.com>
2021-05-24 11:10:48 +0530

    cart remove added, cart add quantity fixed

--- apps/cart/views.py
+++ apps/cart/views.py
@@ -1,6 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from.cart import Cart
 
 def cart_detail(request):
+    cart = Cart(request)
+    remove_from_cart = request.GET.get('remove_from_cart', '')
+    change_quantity = request.GET.get('change_quantity', '')
+    quantity = request.GET.get('quantity', 0)
+
+    if remove_from_cart:
+        cart.remove(remove_from_cart)
+
+        return redirect('cart')
+
+    if change_quantity:
+        cart.add(change_quantity, quantity, True)
+
+        return redirect('cart')
+
     return render(request, 'cart.html')
 
 # Create your views here.
